11790e2efdb2a0c89c73e1a46cb1db58a3c110cc
alexey211204 <alexey.zhuravel2004@gmail.com>
2026-09-17 22:21:21 +0200

    added main and other important files

--- main.tf
+++ main.tf
@@ -1,3 +1,58 @@
 # 1. Deploy an S3 storage bucket 
 
 # 2. Confugure bucket policy to allow grafana iam role to use storage 
+
+resource "aws_s3_bucket" "grafana_backups" {
+  bucket = "mate-task6-tf-bucket"
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.grafana_backups.id
+  policy = data.aws_iam_policy_document.grafana_policy.json
+}
+
+data "aws_iam_policy_document" "grafana_policy" {
+  statement {
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        var.grafana_iam_role_arn
+      ]
+    }
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.grafana_backups.bucket}",
+    ]
+  }
+
+  statement {
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        var.grafana_iam_role_arn
+      ]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.grafana_backups.bucket}/*",
+    ]
+
+  }
+
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,5 @@
-grafana_iam_role_arn = ""
+grafana_iam_role_arn = "arn:aws:iam::976134557491:role/grafana_role"
+
+# grafana_url = "http://18.159.106.177:3000/"
+
+# instance_public_ip = "18.159.106.177"
